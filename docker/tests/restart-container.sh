b69893e9be806f7bdc2751a2248f8574928ff0f1
wileyj <2847772+wileyj@users.noreply.github.com>
2025-09-12 11:01:17 -0700

    add some error checking to the container restart script

--- docker/tests/restart-container.sh
+++ docker/tests/restart-container.sh
@@ -16,6 +16,19 @@ function help(){
 if ! [[ "${container}" && "${sleep_duration}" ]];then
     help
 else
+    # check if the defined container is actually running
+    # this is not a failure mode though, since this check is only that a container is running - not that it may have been stopped
+    #  print a warn and then try to start it after the sleep duration has passed
+    if ! [ -n "$(docker ps -f "name=${container}" -f "status=running" -q )" ]; then
+        echo "WARN: container (${container}) is not running"
+    fi
+    # check if sleep_duration is an integer.
+    # if this is not an integer, print an error and exit (we cannot sleep for a non-integer number of seconds)
+    if ! [ "$sleep_duration" -eq "$sleep_duration" ] 2>/dev/null; then
+        echo "Error: sleep duration (${sleep_duration}) is not a number"
+        echo "Exiting"
+        exit 1
+    fi
     echo "Stopping (${container})"
     docker stop ${container} 1> /dev/null # stop container without printing to stdout (stderr should still print)
     if [ "$?" -ne "0" ]; then
